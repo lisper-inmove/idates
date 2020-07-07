73909a03231fe4f1403f6a9a7719476371ddf077
inmove <lisper.inmove@gmail.com>
2020-07-08 01:16:05 +0800

    done

--- __init__.py
+++ __init__.py
@@ -10,76 +10,53 @@ from collections import namedtuple
 from datetime import datetime
 from datetime import timedelta
 
+import tzlocal
 import pytz
 
 
 class IDates:
-    def __init__(self, timezone="UTC"):
-        self.timezone = pytz.timezone(timezone)
-
-    def timestamp_to_string(self, timestamp, fmt="%Y-%m-%d %H:%M:%S"):
-        """
-        1. timestamp: 时间戳
-        2. fmt: 转换后字符串的格式
-        """
-        date = datetime.fromtimestamp(timestamp)
-        date = date.astimezone(self.timezone)
-        return date.strftime(fmt)
-
-    def string_to_timestamp(self, date, fmt="%Y-%m-%d %H:%M:%S"):
-        """
-        1. date: 字符串格式
-        2. fmt: date必须符合fmt
-        """
-        date = datetime.strptime(date, fmt)
-        date = date.astimezone(self.timezone)
-        return date.timestamp()
-
-    def timestamp_to_datetime(self, timestamp):
-        date = datetime.fromtimestamp(timestamp)
-        date = date.astimezone(self.timezone)
-        return date
-
-    def string_to_datetime(self, date, fmt="%Y-%m-%d %H:%M:%S"):
-        date = datetime.strptime(date, fmt)
-        date = date.astimezone(self.timezone)
-        return date
-
-    def datetime_to_string(self, date, fmt="%Y-%m-%d %H:%M:%S"):
-        date = date.astimezone(self.timezone)
-        return date.strftime(fmt)
-
-    def get_timestamp_range(self, start_timestamp, delta, is_zero=True):
-        TimeRange = namedtuple("TimeRange", ["start", "end"])
-        delta = timedelta(delta)
-        date = self.timestamp_to_datetime(start_timestamp)
-        date2 = date + delta
-        if is_zero:
-            date = date.replace(hour=0, minute=0, second=0)
-            date2 = date2.replace(hour=0, minute=0, second=0)
-        if delta.days >= 0:
-            return TimeRange(start=date.timestamp(), end=date2.timestamp())
+    def __init__(self, timestamp=None, date=None, fmt="%Y-%m-%d %H:%M:%S"):
+        self._local_tz = tzlocal.get_localzone()
+        if timestamp is None and date is None:
+            self.date = datetime.now(self._local_tz)
+        elif timestamp is not None:
+            self.init_from_timestamp(timestamp)
+        elif date is not None:
+            self.init_from_string(date, fmt)
         else:
-            return TimeRange(start=date2.timerange(), end=date.timestamp())
+            raise Exception("Please give me @timestamp or @date")
 
-    def get_date_range(self, start_timestamp, end_timestamp, fmt="%Y-%m-%d %H:%M:%S"):
-        return self.get_date_range_steps(start_timestamp, end_timestamp, fmt, 1)
+    def init_from_timestamp(self, timestamp):
+        self.date = datetime.fromtimestamp(timestamp)
 
-    def get_date_range_steps(self, start_timestamp, end_timestamp, fmt="%Y-%m-%d %H:%M:%S", steps=1):
-        DateRange = namedtuple("DateRange", ["start", "end"])
-        start_date = self.timestamp_to_datetime(start_timestamp)
-        end_date = self.timestamp_to_datetime(end_timestamp)
-        days = (end_date - start_date).days
-        while days - steps > 0:
-            next_date = start_date + timedelta(steps)
-            start_date_str = self.datetime_to_string(start_date, fmt=fmt)
-            next_date_str = self.datetime_to_string(next_date, fmt=fmt)
-            yield DateRange(start=start_date_str, end=next_date_str)
-            start_date += timedelta(steps)
-            days -= steps
+    def init_from_string(self, date, fmt):
+        self.date = datetime.strptime(date, fmt)
 
-        if days > 0:
-            next_date = start_date + timedelta(abs(days))
-            start_date_str = self.datetime_to_string(start_date, fmt=fmt)
-            next_date_str = self.datetime_to_string(next_date, fmt=fmt)
-            yield DateRange(start=start_date_str, end=next_date_str)
+    def timestamp(self):
+        return self.date.timestamp()
+
+    def string(self, fmt="%Y-%m-%d %H:%M:%S"):
+        return self.date.strftime(fmt)
+
+    def timestamp_to_tz(self, to_tz):
+        to_tz = pytz.timezone(to_tz)
+        return to_tz.localize(self.date).timestamp()
+
+    def string_to_tz(self, to_tz, fmt="%Y-%m-%d %H:%M:%S"):
+        to_tz = pytz.timezone(to_tz)
+        date = to_tz.localize(self.date)
+        return date.strftime(fmt)
+
+if __name__ == '__main__':
+    import time
+    obj = IDates(date="2020-09-01", fmt="%Y-%m-%d")
+    print(obj.timestamp())
+    print(obj.timestamp_to_tz("UTC"))
+    print(obj.string())
+    print(obj.string_to_tz("UTC"))
+
+    obj = IDates(timestamp=1598889600)
+    print(obj.timestamp())
+    print(obj.timestamp_to_tz("UTC"))
+    print(obj.string())
+    print(obj.string_to_tz("UTC"))
